8e34e26cfa65923fdc3456b2c4e5d0838dced6f5
GITHUBETERNAL <63897581+GITHUBETERNAL@users.noreply.github.com>
2023-02-15 19:27:24 +0530

    Update Go design pattern Explained.go

--- Go design pattern Explained.go
+++ Go design pattern Explained.go
@@ -1,43 +1,50 @@
 package main
 
 import {
-"fmt"
-"ioioutil"
-"net/http"
-"time"
+  "fmt"
+  "ioioutil"
+  "net/http"
+  "time"
 }
 var timeoutMilliseconds int 3000
 
 type barrierResp struct {
-Err error
-Resp string
+  Err error
+  Resp string
 }
 
-func main() 
+func main() {
+      endpoints = []string{"http://httpbin.org/headers", "http://regres.in/api/products/3"}8
+  barrier(endpoints...)
+  
+}
 
 func barrier(endpoints ...string) {
-requestNumber := len(endpoints)
+  requestNumber := len(endpoints)
 
-in := make(chan barrierResp, requestNumber)
+  in := make(chan barrierResp, requestNumber)
+  defer close(in)
+  responses := make([]barrierResp, requestNumber)
+  
+  
+    for _, endpoint := range endpoints {
+      gor makeRequest(in, endpoints)
+    }
 
-for _, endpoint := range endpoints {
-gor makeRequest(in, endpoints)
-}
+    var hasError bool 
+    for i := 0; i < requestNumber; i++ {
+      resp := <- in
+        if resp .Err != nil {
+          fmt.Println("ERROR: ", resp.Err)
+          hasError = true
+          break
+        }
+      response[i] = resp
+      }
 
-var hasError bool 
-for i := 0; i < requestNumber; i++ {
-resp := <- in
-if resp .Err != nil {
-fmt.Println("ERROR: ", resp.Err)
-hasError = true
-break
-}
-response[i] = resp
-}
-
-if !hasError {
-for _, resp := range responses {
-fmt.Println(resp.Resp)
-}
-}
+    if !hasError {
+      for _, resp := range responses {
+      fmt.Println(resp.Resp)
+      }
+   }
 }
